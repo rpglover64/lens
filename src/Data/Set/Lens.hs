f5e972e87a66a8e38b0abd5d9a94795d220219e1
Alex R <rpglover64@gmail.com>
2015-07-22 13:40:06 -0400

    Possibly fix documentation typo in Data.Set.Lens
    It looks like the reference in `setmapped` should be to `setOf`; if this is not the case, then feel free to close this PR.

--- src/Data/Set/Lens.hs
+++ src/Data/Set/Lens.hs
@@ -33,7 +33,7 @@ import Data.Set as Set
 -- the elements to new values.
 --
 -- Sadly, you can't create a valid 'Traversal' for a 'Set', but you can
--- manipulate it by reading using 'Control.Lens.Fold.folded' and reindexing it via 'setmapped'.
+-- manipulate it by reading using 'Control.Lens.Fold.folded' and reindexing it via 'setOf'.
 --
 -- >>> over setmapped (+1) (fromList [1,2,3,4])
 -- fromList [2,3,4,5]
